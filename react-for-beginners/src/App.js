7b2f967d6372b6474d86c68e252e780f0871f40f
gwanu-dev <devkwanwoo@gmail.com>
2024-01-21 22:36:28 +0900

    [#7.1 / 240121] ToDoList Part 2

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -16,19 +16,20 @@ function App() {
       return;
     }
     setToDo("");
-    //toDos = "" DONT EVER MODIFY THE VALUE/STATE DIRECTLY!!!
-    // WE DO MODIFY THE VALUE BY THE FUNCTION!!
+    
     setToDos(currentState => [toDo, ...currentState]);
     console.log(toDo);
   }
+  console.log(toDos);
   return (
     <div>
       <h1>MY DO TOS ({toDos.length})</h1>
-      {toDos.map((a, i) => { return <h3>{toDos[i]}</h3> })}
+      {/* {toDos.map((a, i) => { return <h3>{toDos[i]}</h3> })} */}
       < form onSubmit={onSubmit}>
         <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do ..." />
         <button>Add To Do</button>
       </form>
+      {toDos.map((item, index) => <li key={index}>{item}</li>)}
     </div >
   );
 }
