5619a1356190dae4007e29b5f3c03a2d4f40f283
gwanu-dev <devkwanwoo@gmail.com>
2024-01-22 00:32:36 +0900

    [#7.2 / 240121] Coin Tracker

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,36 +1,28 @@
-import Button from "./Button";
+
 import styles from "./App.module.css"
 import { useState, useEffect } from 'react';
 
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => {
-    setToDo(event.target.value);
-  }
-
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDo("");
-    
-    setToDos(currentState => [toDo, ...currentState]);
-    console.log(toDo);
-  }
-  console.log(toDos);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([])
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then(response => response.json())
+      .then(json => {
+        console.log(json)
+        setCoins(json);
+        setLoading(false);
+      });
+  })
   return (
     <div>
-      <h1>MY DO TOS ({toDos.length})</h1>
-      {/* {toDos.map((a, i) => { return <h3>{toDos[i]}</h3> })} */}
-      < form onSubmit={onSubmit}>
-        <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do ..." />
-        <button>Add To Do</button>
-      </form>
-      {toDos.map((item, index) => <li key={index}>{item}</li>)}
-    </div >
+      <h2>The Coins! ({coins.length})</h2>
+      {loading ? <strong> loading... </strong> : null}
+      <ul>
+        {/* {coins.map((coin) => <li key={coin.username}>{coin.name} ({coin.username}) : {coin.email} </li>)} */}
+      </ul>
+    </div>
   );
 }
 
